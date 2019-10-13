276edbfff0ea82a9402501f2c2d24a8d9ec4df7c
guruliciousjide@gmail.com <guruliciousjide@gmail.com>
2019-10-13 21:22:41 +0100

    Fix

--- lib/ui/post.dart
+++ lib/ui/post.dart
@@ -9,56 +9,61 @@ class Post extends StatefulWidget {
 
 class _PostState extends State<Post> {
   TextEditingController _name = TextEditingController();
+  bool loading = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(),
         body: Column(
           children: <Widget>[
-           Material(
-                        child: Container(
-                  decoration: BoxDecoration(
-                    //color: Colors.blue,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  height: (MediaQuery.of(context).size.height) * 0.075,
-                  width: (MediaQuery.of(context).size.width) / 0.02,
-                  child: Padding(
-                    padding:
-                        const EdgeInsets.only(top: 8.0, left: 8.0, right: 8.0),
-                    child: TextField(
-                      controller:_name,
-                      decoration: InputDecoration(
-                          border: InputBorder.none, hintText: "Enter something"),
-                    
-                    ),
+            Material(
+              child: Container(
+                decoration: BoxDecoration(
+                  //color: Colors.blue,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                height: (MediaQuery.of(context).size.height) * 0.075,
+                width: (MediaQuery.of(context).size.width) / 0.02,
+                child: Padding(
+                  padding:
+                  const EdgeInsets.only(top: 8.0, left: 8.0, right: 8.0),
+                  child: TextField(
+                    controller:_name,
+                    decoration: InputDecoration(
+                        border: InputBorder.none, hintText: "Enter something"),
+
                   ),
                 ),
-           ),
-           RaisedButton(
-             onPressed: (){
-               _addPost();
-             },
-             child: Text('Send'),
-           )
+              ),
+            ),
+
+            loading
+                ?Center(child: CircularProgressIndicator(),)
+                :RaisedButton(
+              onPressed: (){
+                _addPost();
+              },
+              child: Text('Send'),
+            ),
             
           ],
         ));
   }
+
   _addPost(){
-  //  final genre = Genre(
-  //    genreName: _name.text,
-  //  ); 
-  final genre = {
-    "name":_name.text,
-  };
-    Api.addGenre(genre).then((success){
+    setState((){
+      loading = true;
+    });
+    Api.addGenre(_name.text).then((success){
+      setState((){
+        loading = false;
+      });
       String title, text;
       if(success){
         title = "Success";
         text = "Your post has been successfully submitted";
       }else{
-         title = "Error";
+        title = "Error";
         text = "An error occurred";
       }
     });
